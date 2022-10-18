cbbbf4029d9b563b28c9e9941230cec6081af72b
kleberlucio <kleber@tron.com.br>
2022-10-18 08:36:53 -0300

    Atualizações

--- Automacao/Execution_Plan/PlanoDeExecucao_Kleber_Server01.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, '/GitHub/Auto_TGC/Automacao/Kleber_Server01/Escrita_Fiscal/Geracao_De_Informacoes_Oficiais/SPED_Fiscal')
-
-from Geracao_SPED_Fiscal import *
-
-#Escrita Fiscal
-#   Geracao_De_Informacoes_Oficiais
-#       SPED_Fiscal
-Redmine_108805()
