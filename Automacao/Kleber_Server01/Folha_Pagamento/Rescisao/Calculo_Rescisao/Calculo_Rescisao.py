5530fd6649f19b4b55366c85b5401ad7a82276a0
Johnathan-fr01 <johnathan.f1@hotmail.com>
2022-10-18 11:11:03 -0300

    Atualizações 18/10/2022
    Foi alterado o método comum VerificaEmpresaPeriodoSelecionado

--- Automacao/Kleber_Server01/Folha_Pagamento/Rescisao/Calculo_Rescisao/Calculo_Rescisao.py
+++ Automacao/Kleber_Server01/Folha_Pagamento/Rescisao/Calculo_Rescisao/Calculo_Rescisao.py
@@ -4,35 +4,36 @@ import sys
 
 def Redmine_105():
     try:
-        GeraLog(False,"Iniciando os teste da Folha")
-        if not PreparaAmbiente('108805', False, '\Folha'):
-            sys.exit(1)
-        if not SelecionaEmpresa(13,True,'Folha'):
-            sys.exit(1)
+        GeraLog(False, "Iniciando os teste da Folha - Redmine_105")
         
-        SelecionaPeriodo(2022,10,2022,False,True)
+        PreparaAmbiente('108805', False, '\Folha')
+        SelecionaEmpresa(13, True, 'Folha')        
+        SelecionaPeriodo(2022, 10, 2022, False, True)      
+        VerificaEmpresaPeriodoSelecionado('13','10','2022')
+        CalcularRescisao(3, 5, 171)
         
-        if not VerificaEmpresaPeriodoSelecionado('13','10','2022'):
-            sys.exit(1)
-
-        CalcularRescisao(3,5,171)
-        pyautogui.press(['enter','enter','enter'])
-        pyautogui.typewrite('05102022')
-        pyautogui.press('enter')
-        pyautogui.typewrite('05102022')
-        pyautogui.press('enter')
-        pyautogui.typewrite('1')
-        pyautogui.press('enter')
-        time.sleep(8)
+        #pyautogui.press(['enter','enter','enter'])
+        PressionarTeclas('enter')
+        PressionarTeclas('enter')
+        PressionarTeclas('enter')
+        
+        EscreverTexto('05102022')
+        PressionarTeclas('enter')
+        EscreverTexto('05102022')
+        PressionarTeclas('enter')
+        EscreverTexto('1')
+        PressionarTeclas('enter')
+        EsperarTempo(8)
         
         if not ExisteImagem('C:\\GitHub\\Auto_TGC\\Automacao\\Kleber_Server01\\Folha_Pagamento\\Rescisao\\Calculo_Rescisao\\img\\Redmine_101010_calculo_rescisao.png', 5):
-            GeraLog(False,"ERRO - Cálculo da rescisão errado")
+            GeraLog(False, "ERRO - Cálculo da rescisão errado")
             return False
     except:
         GeraLog(False, "Interrompido os testes sobre o cenário 100000, verificar LOG acima")
+        sys.exit(1)
     GeraLog(False,"Terminado os teste da Folha")
     return True
 
-#pyautogui.click(1779,29)
-#SelecionaEmpresa(13,True)
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 Redmine_105()
